8973029b3c6d71105fc66b7fe7b61748f136e275
khohoh <ianjeong0820@gmail.com>
2021-10-26 00:48:00 -0700

    commit

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -38,8 +38,8 @@ class PostsController extends Controller
         ]);
 
         $imagePath = request('image')->store('uploads', 'public');
-        dd(public_path("storage/{$imagePath}"));
-        $image = Image::make(public_path("storage/{$imagePath}"))->fit(1200, 1200);
+        //dd(public_path("storage/{$imagePath}"));
+        $image = Image::make(public_path("storage/{$imagePath}"));//->fit(1200, 1200);
         
         $image->save();
 
